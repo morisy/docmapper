50a01823bc17e4cb2940f2800e7b3315cc78d683
Michael Morisy <m@morisy.com>
2023-03-31 20:20:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,86 @@
-"""
-This is a hello world add-on for DocumentCloud.
-
-It demonstrates how to write a add-on which can be activated from the
-DocumentCloud add-on system and run using Github Actions.  It receives data
-from DocumentCloud via the request dispatch and writes data back to
-DocumentCloud using the standard API
-"""
-
+import json
+import csv
+import tempfile
+from commonregex import CommonRegex
 from documentcloud.addon import AddOn
+import folium
+from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
 
 
-class HelloWorld(AddOn):
-    """An example Add-On for DocumentCloud."""
+class AddressMap(AddOn):
+    """AddressMap AddOn class"""
 
     def main(self):
-        """The main add-on functionality goes here."""
-        # fetch your add-on specific data
-        name = self.data.get("name", "world")
+        addresses = []
 
-        self.set_message("Hello World start!")
-
-        # add a hello note to the first page of each selected document
+        # Extract addresses from documents
         for document in self.get_documents():
-            # get_documents will iterate through all documents efficiently,
-            # either selected or by query, dependeing on which is passed in
-            document.annotations.create(f"Hello {name}!", 0)
+            for page in range(1, document.pages + 1):
+                text = document.get_page_text(page)
+                parsed_text = CommonRegex(text)
+                street_addresses = set(parsed_text.street_addresses)
 
-        with open("hello.txt", "w+") as file_:
-            file_.write("Hello world!")
-            self.upload_file(file_)
+                for address in street_addresses:
+                    # Create annotations for extracted addresses
+                    annotation = document.annotations.create(
+                        "Address found on this page",
+                        page - 1,
+                        content=address,
+                        access=self.data.get("access"),
+                    )
 
-        self.set_message("Hello World end!")
-        self.send_mail("Hello World!", "We finished!")
+                    addresses.append(
+                        {
+                            "document": document.title,
+                            "page": page,
+                            "url": annotation.canonical_url,
+                            "address": address,
+                        }
+                    )
+
+        # Generate a CSV file with extracted addresses
+        csv_file = tempfile.NamedTemporaryFile(mode="w+", delete=False, suffix=".csv")
+        fieldnames = ["document", "page", "url", "address"]
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(addresses)
+        csv_file.close()
+
+        # Upload the CSV file
+        self.upload_file(open(csv_file.name))
+
+        # Geocode addresses
+        geolocator = Nominatim(user_agent="address_mapper")
+        geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
+        locations = [
+            (geocode(addr["address"]), addr["document"], addr["page"], addr["url"])
+            for addr in addresses
+        ]
+
+        # Remove any None values in locations
+        locations = [loc for loc in locations if loc[0] is not None]
+
+        # Create a folium map
+        first_location = locations[0][0]
+        map = folium.Map(location=[first_location.latitude, first_location.longitude], zoom_start=12)
+
+        # Add markers to the map
+        for location, doc_title, page, url in locations:
+            folium.Marker(
+                [location.latitude, location.longitude],
+                popup=f"{doc_title}<br>Page: {page}<br><a href='{url}' target='_blank'>View Annotation</a>",
+            ).add_to(map)
+
+        # Save the map as an HTML file and upload
+        map_file = tempfile.NamedTemporaryFile(mode="w+", delete=False, suffix=".html")
+        map.save(map_file.name)
+        map_file.close()
+
+        self.upload_file(open(map_file.name), "addresses_map.html")
+
+        self.set_message("Address extraction and map generation completed")
 
 
 if __name__ == "__main__":
-    HelloWorld().main()
+    AddressMap().main()
