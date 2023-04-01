08e1e0bac7579b19a0191e54b8e0657b8a3c6888
Michael Morisy <m@morisy.com>
2023-03-31 20:50:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,86 +1,98 @@
-import json
 import csv
-import tempfile
-from commonregex import CommonRegex
-from documentcloud.addon import SoftTimeOutAddOn
 import folium
+import geopy
 from geopy.geocoders import Nominatim
-from geopy.extra.rate_limiter import RateLimiter
+from zipfile import ZipFile
+import os
+
+from documentcloud.addon import SoftTimeOutAddOn
+from commonregex import CommonRegex
+
+geolocator = Nominatim(user_agent="address_mapper")
 
 
-class AddressMap(SoftTimeOutAddOn):
-    """AddressMap AddOn class"""
+class AddressMapper(SoftTimeOutAddOn):
+    """Extracts addresses from documents, creates annotations, and generates a map with links to the annotations."""
+
+    def __init__(self):
+        super().__init__()
+
+    def detect(self, document, page, parsed, positions):
+        addresses = []
+        for word in set(parsed):
+            for info in positions:
+                if word in info["text"]:
+                    location = geolocator.geocode(word)
+                    if location:
+                        annotation = document.annotations.create(
+                            "Address found",
+                            page - 1,
+                            content=word,
+                            access=self.data.get("access"),
+                            x1=info["x1"],
+                            y1=info["y1"],
+                            x2=info["x2"],
+                            y2=info["y2"],
+                        )
+                        addresses.append(
+                            {
+                                "address": word,
+                                "latitude": location.latitude,
+                                "longitude": location.longitude,
+                                "document_title": document.title,
+                                "page": page,
+                                "annotation_id": annotation.id,
+                            }
+                        )
+        return addresses
 
     def main(self):
-        addresses = []
+        all_addresses = []
 
-        # Extract addresses from documents
+        # Extract addresses and create annotations
         for document in self.get_documents():
             for page in range(1, document.pages + 1):
                 text = document.get_page_text(page)
                 parsed_text = CommonRegex(text)
-                street_addresses = set(parsed_text.street_addresses)
 
-                for address in street_addresses:
-                    # Create annotations for extracted addresses
-                    annotation = document.annotations.create(
-                        "Address found on this page",
-                        page - 1,
-                        content=address,
-                        access=self.data.get("access"),
-                    )
+                positions = document.get_page_text(page, positions=True)
 
-                    addresses.append(
-                        {
-                            "document": document.title,
-                            "page": page,
-                            "url": annotation.canonical_url,
-                            "address": address,
-                        }
-                    )
+                addresses = set(parsed_text.street_addresses) | set(parsed_text.po_boxes)
+                found_addresses = self.detect(document, page, addresses, positions)
+                all_addresses.extend(found_addresses)
 
-        # Generate a CSV file with extracted addresses
-        csv_file = tempfile.NamedTemporaryFile(mode="w+", delete=False, suffix=".csv")
-        fieldnames = ["document", "page", "url", "address"]
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(addresses)
-        csv_file.close()
+        # Create a CSV file with the extracted addresses
+        with open("addresses.csv", "w", newline="") as csvfile:
+            fieldnames = ["address", "latitude", "longitude", "document_title", "page", "annotation_id"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(all_addresses)
 
-        # Upload the CSV file
-        self.upload_file(open(csv_file.name))
+        # Generate a map with the extracted addresses using Stamen Toner tiles
+        m = folium.Map(tiles="Stamen Toner")
 
-        # Geocode addresses
-        geolocator = Nominatim(user_agent="address_mapper")
-        geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
-        locations = [
-            (geocode(addr["address"]), addr["document"], addr["page"], addr["url"])
-            for addr in addresses
-        ]
-
-        # Remove any None values in locations
-        locations = [loc for loc in locations if loc[0] is not None]
-
-        # Create a folium map
-        first_location = locations[0][0]
-        map = folium.Map(location=[first_location.latitude, first_location.longitude], zoom_start=12)
-
-        # Add markers to the map
-        for location, doc_title, page, url in locations:
+        for address in all_addresses:
             folium.Marker(
-                [location.latitude, location.longitude],
-                popup=f"{doc_title}<br>Page: {page}<br><a href='{url}' target='_blank'>View Annotation</a>",
-            ).add_to(map)
+                location=[address["latitude"], address["longitude"]],
+                popup=f'<a href="https://www.documentcloud.org/documents/{address["annotation_id"]}/annotations/{address["annotation_id"]}.html" target="_blank">{address["address"]}</a>',
+            ).add_to(m)
 
-        # Save the map as an HTML file and upload
-        map_file = tempfile.NamedTemporaryFile(mode="w+", delete=False, suffix=".html")
-        map.save(map_file.name)
-        map_file.close()
+        m.save("map.html")
 
-        self.upload_file(open(map_file.name), "addresses_map.html")
+        # Zip the map HTML and CSV of exported addresses
+        with ZipFile("address_map_export.zip", "w") as zipped_export:
+            zipped_export.write("map.html")
+            zipped_export.write("addresses.csv")
 
-        self.set_message("Address extraction and map generation completed")
+        # Upload the zip file
+        with open("address_map_export.zip", "rb") as file:
+            self.upload_file(file)
+
+        # Clean up
+        os.remove("map.html")
+        os.remove("addresses.csv")
+        os.remove("address_map_export.zip")
 
 
 if __name__ == "__main__":
-    AddressMap().main()
+    AddressMapper().main()
